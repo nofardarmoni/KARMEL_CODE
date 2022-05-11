1efe13fbdb9d8f71fd1076ecbb513536dae9dee0
Ronit Priav <ronit.priav@appricots.onmicrosoft.com>
2022-05-11 12:30:07 +0300

    water fixed icon usage and prettify

--- src/layers/Water/WaterLayer.js
+++ src/layers/Water/WaterLayer.js
@@ -4,23 +4,25 @@ import { deepCompareMemo, polygonToWKT } from "@services";
 import { makeStyles } from "@material-ui/core";
 import { icons } from "./waterIcons";
 import { CustomPopup } from "@core";
-import { useApiQuery } from "@hooks/useApiQuery";
+// import { useApiQuery } from "@hooks/useApiQuery";
 
-const data = [{
-    WATER_KEY: '1',
-    WATER_TAAGID: 'הרי נצרת',
-    WATER_RASHUT: 'נצרת',
-    WATER_NZRIGHT: '32.69764094981775',
-    WATER_NZLEFT: '32.697639465332',
+const data = [
+  {
+    WATER_KEY: "1",
+    WATER_TAAGID: "הרי נצרת",
+    WATER_RASHUT: "נצרת",
+    WATER_NZRIGHT: "32.69764094981775",
+    WATER_NZLEFT: "32.697639465332",
     WATER_MAHOZ: "חיפה",
     WATER_TUSHVIM: "80000",
     WATER_TUSHVIM_NO_WATER: "56000",
     WATER_NEZEK_BAMITKAN: "סדקים",
     WATER_RAMAT_TIFKUD_PRECENT: "0.300000011920929",
-    WATER_RAMAT_HERUM: '4',
-    WATER_TAHANUT_HALUKA:'40',
-    WATER_ZMAN_TIKUN_DAYS: '10'
-  }]
+    WATER_RAMAT_HERUM: "4",
+    WATER_TAHANUT_HALUKA: "40",
+    WATER_ZMAN_TIKUN_DAYS: "10",
+  },
+];
 
 const useStyles = makeStyles(() => ({
   title: {
@@ -47,49 +49,41 @@ const getDate = (lastUpdateTime) => {
   return `${day}/${month}/${year}`;
 };
 
-
 function WaterLayer({ layerKey, label, polygon, distance, showId }) {
   const classes = useStyles();
   const layerCache = useRef([]);
 
   const polygonWKT = useMemo(() => polygonToWKT(polygon), [polygon]);
 
-//   const { data } = useApiQuery({
-//     dataPath: `water/${polygon ? "polygon/" : ""}${layerKey}`,
-//     label,
-//     body: polygon && {
-//       polygon: polygonWKT,
-//       distance,
-//     },
-//     options: {
-//       placeholderData: layerCache.current,
-//       onSuccess: (data) => (layerCache.current = data),
-//     },
-//   });
+  //   const { data } = useApiQuery({
+  //     dataPath: `water/${polygon ? "polygon/" : ""}${layerKey}`,
+  //     label,
+  //     body: polygon && {
+  //       polygon: polygonWKT,
+  //       distance,
+  //     },
+  //     options: {
+  //       placeholderData: layerCache.current,
+  //       onSuccess: (data) => (layerCache.current = data),
+  //     },
+  //   });
 
   if (!data) return null;
   return (
     <>
-      {data.map(
-        (waterStation) =>
-           (
-            <Marker
-              key={waterStation.WATER_KEY}
-              position={[waterStation.WATER_NZRIGHT, waterStation.WATER_NZLEFT]}
-              icon={
-                icons["c"]["לא ידוע"] ??
-                icons[layerKey]?.default ??
-                icons.default
-              }
-            >
-              <CustomPopup closeButton={false}>
-                <div className={classes.title}>מחוז:  {waterStation.WATER_MAHOZ}</div>
-              
-            
-              </CustomPopup>
-            </Marker>
-          )
-      )}
+      {data.map((waterStation) => (
+        <Marker
+          key={waterStation.WATER_KEY}
+          position={[waterStation.WATER_NZRIGHT, waterStation.WATER_NZLEFT]}
+          icon={icons["waterIcon"]}
+        >
+          <CustomPopup closeButton={false}>
+            <div className={classes.title}>
+              מחוז: {waterStation.WATER_MAHOZ}
+            </div>
+          </CustomPopup>
+        </Marker>
+      ))}
     </>
   );
 }
--- src/layers/Water/waterIcons.js
+++ src/layers/Water/waterIcons.js
@@ -1,62 +1,10 @@
 import L from "leaflet";
 
-const iconsUrl = "icons/layers/waters/";
+const iconsUrl = "icons/layers/waters";
 
 export const icons = {
-  c: {
-    "לא ידוע": L.icon({
-      iconUrl: `${iconsUrl}water-drop.png`,
-      iconSize: [25, 30],
-    }),
-    סגור: L.icon({
-      iconUrl: `${iconsUrl}clalit/clalit-red.png`,
-      iconSize: [25, 30],
-    }),
-    פתוח: L.icon({
-      iconUrl: `${iconsUrl}clalit/clalit-green.png`,
-      iconSize: [25, 30],
-    }),
-  },
-  לאומית: {
-    "לא ידוע": L.icon({
-      iconUrl: `${iconsUrl}leumit/leumit-gray.png`,
-      iconSize: [25, 30],
-    }),
-    סגור: L.icon({
-      iconUrl: `${iconsUrl}leumit/leumit-red.png`,
-      iconSize: [25, 30],
-    }),
-    פתוח: L.icon({
-      iconUrl: `${iconsUrl}leumit/leumit-green.png`,
-      iconSize: [25, 30],
-    }),
-  },
-  מאוחדת: {
-    "לא ידוע": L.icon({
-      iconUrl: `${iconsUrl}meuhedet/meuhedet-gray.png`,
-      iconSize: [25, 30],
-    }),
-    סגור: L.icon({
-      iconUrl: `${iconsUrl}meuhedet/meuhedet-red.png`,
-      iconSize: [25, 30],
-    }),
-    פתוח: L.icon({
-      iconUrl: `${iconsUrl}meuhedet/meuhedet-green.png`,
-      iconSize: [25, 30],
-    }),
-  },
-  מכבי: {
-    "לא ידוע": L.icon({
-      iconUrl: `${iconsUrl}maccabi/maccabi-gray.png`,
-      iconSize: [25, 30],
-    }),
-    סגור: L.icon({
-      iconUrl: `${iconsUrl}maccabi/maccabi-red.png`,
-      iconSize: [25, 30],
-    }),
-    פתוח: L.icon({
-      iconUrl: `${iconsUrl}maccabi/maccabi-green.png`,
-      iconSize: [25, 30],
-    }),
-  },
+  'waterIcon': L.icon({
+    iconUrl: `${iconsUrl}/water-drop.png`,
+    iconSize: [25, 30],
+  }),
 };
