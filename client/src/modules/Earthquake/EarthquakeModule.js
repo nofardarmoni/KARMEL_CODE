c65dcbe029731b983c7717757418f26cbc1e0e74
Shay Ronen <47533456+wwwshayronen@users.noreply.github.com>
2022-06-18 17:18:37 +0300

    magnitode alert, icons colors, border color when state change, return "" when level of fucntionng is 100

--- client/src/modules/Earthquake/EarthquakeModule.js
+++ client/src/modules/Earthquake/EarthquakeModule.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { useRecoilValue, useRecoilState } from "recoil";
 import {
-  LayerDisplayer, Map, SearchGeocoder, EventTitle,
+  LayerDisplayer,
+  Map,
+  SearchGeocoder,
+  EventTitle,
   OpenEventsTitle,
 } from "@features";
 
@@ -11,48 +14,131 @@ import { getLocalStorage, setLocalStorage } from "@services";
 import { currentModuleState } from "@states/moduleState";
 import { layers as moduleLayers } from "./layers";
 import { currentEventState, eventListState } from "@states/eventState";
-import { earthquakeState, magnitodeState } from "../../states/earthquakeState"
-
+import { earthquakeState, magnitodeState } from "../../states/earthquakeState";
 
 function MapChildren({ layers, setLayers }) {
-  const [showMagnitodaInput, setShowMagnitodaInput] = useState(false)
-  const [magnitode, setMagnitode] = useState(0)
-  const [newEarthquakeState, setNewEarthquakeState] = useRecoilState(earthquakeState)
-  const [newMagnitodeState, setNewMagnitodeState] = useRecoilState(magnitodeState)
-  const predictValue = useRecoilValue(earthquakeState)
+  const [showMagnitodaInput, setShowMagnitodaInput] = useState(false);
+  const [magnitode, setMagnitode] = useState(0);
+  const [newEarthquakeState, setNewEarthquakeState] = useRecoilState(
+    earthquakeState
+  );
+  const [newMagnitodeState, setNewMagnitodeState] = useRecoilState(
+    magnitodeState
+  );
+  const predictValue = useRecoilValue(earthquakeState);
+  const [whiteBorderRealTimeBtn, setWhiteBorderRealTimeBtn] = useState("black");
+  const [whiteBorderPredictBtn, setWhiteBorderPredictBtn] = useState("black");
 
-  useEffect(() => {
-    console.log("showMagnitodaInput: ", showMagnitodaInput)
-    console.log("newEarthquakeState: ", newEarthquakeState)
+  // useEffect(() => {
+  //   console.log("newEarthquakeState: ", newEarthquakeState)
+  //   if (earthquakeState === "realtime") {
+  //     setWhiteBorderRealTimeBtn("white");
+  //     setWhiteBorderPredictBtn("black");
+  //   } else if (earthquakeState === "predictMode") {
+  //     setWhiteBorderPredictBtn("white");
+  //     setWhiteBorderRealTimeBtn("black");
+  //   }
+  // }, [newEarthquakeState]);
 
-  }, [showMagnitodaInput, newEarthquakeState])
-
-  const handlePredictionOnClick = () => {
-    setShowMagnitodaInput(true)
-
-    if (newEarthquakeState === 'realtime') {
-      setNewEarthquakeState('predictMode')
+  const handlePredictionOnClick = (checkMagnitode= true) => {
+    if (checkMagnitode && (magnitode < 4.5 || magnitode > 10)) {
+      alert("יש להכניס מגניטודה בין 4.5-10");
+      setMagnitode(0);
     }
 
-    setNewMagnitodeState(magnitode)
-  }
+    setShowMagnitodaInput(true);
+
+    if (newEarthquakeState === "realtime") {
+      setWhiteBorderPredictBtn("white");
+      setWhiteBorderRealTimeBtn("black");
+      setNewEarthquakeState("predictMode");
+    }
+
+    setNewMagnitodeState(magnitode);
+  };
 
   const handlePicStateOnClick = () => {
-    setNewEarthquakeState('realtime')
-    setShowMagnitodaInput(false)
-
-  }
-
+    setNewEarthquakeState("realtime");
+    setWhiteBorderRealTimeBtn("white");
+    setWhiteBorderPredictBtn("black");
+    setShowMagnitodaInput(false);
+  };
 
   return (
     <>
-      <Toolbox layers={layers} setLayers={setLayers} />MuiButtonBase-root MuiFab
-      <button onClick={handlePicStateOnClick} className="-root MuiSpeedDial-fab makeStyles-speedDialFab-44 MuiFab-primary"
-        style={{ position: 'absolute', top: '20px', left: '85px', zIndex: '1000', height: '50px', backgroundColor: 'rgb(25 25 25)', borderRadius: '5px', width: '100px' }}>תמונת מצב</button>
-      <button onClick={handlePredictionOnClick} className="MuiButtonBase-root MuiFab-root MuiSpeedDial-fab makeStyles-speedDialFab-44 MuiFab-primary"
-        style={{ position: 'absolute', top: '20px', left: '200px', zIndex: '1000', height: '50px', backgroundColor: 'rgb(25 25 25)', borderRadius: '5px', width: '100px' }}>חיזוי</button>
-      <input value={magnitode} min={4.5} max={13} onChange={(e) => setMagnitode(e.target.value)} placeholder="מגניטודה" type="text" style={{ display: showMagnitodaInput ? 'block' : 'none', position: 'absolute', top: '20px', left: '350px', zIndex: '1000', height: '50px', backgroundColor: 'rgb(25 25 25)', borderRadius: '5px', width: '100px', color: 'white' }} />
-      <button style={{ display: showMagnitodaInput ? 'block' : 'none', position: 'absolute', top: '80px', left: '350px', zIndex: '500', height: '20px', backgroundColor: 'rgb(25 25 25)', borderRadius: '5px', width: '100px', color: 'white' }} onClick={handlePredictionOnClick}>הפעל חיזוי</button>
+      <Toolbox layers={layers} setLayers={setLayers} />
+      MuiButtonBase-root MuiFab
+      <button
+        onClick={handlePicStateOnClick}
+        className="-root MuiSpeedDial-fab makeStyles-speedDialFab-44 MuiFab-primary"
+        style={{
+          position: "absolute",
+          top: "20px",
+          left: "85px",
+          zIndex: "1000",
+          height: "50px",
+          backgroundColor: "rgb(25 25 25)",
+          borderRadius: "5px",
+          width: "100px",
+          border: `1px solid ${whiteBorderRealTimeBtn}`,
+        }}
+      >
+        תמונת מצב
+      </button>
+      <button
+        onClick={() => handlePredictionOnClick(false)}
+        className="MuiButtonBase-root MuiFab-root MuiSpeedDial-fab makeStyles-speedDialFab-44 MuiFab-primary"
+        style={{
+          position: "absolute",
+          top: "20px",
+          left: "200px",
+          zIndex: "1000",
+          height: "50px",
+          backgroundColor: "rgb(25 25 25)",
+          borderRadius: "5px",
+          width: "100px",
+          border: `1px solid ${whiteBorderPredictBtn}`,
+        }}
+      >
+        חיזוי
+      </button>
+      <input
+        value={magnitode}
+        min={4.5}
+        max={13}
+        onChange={(e) => setMagnitode(e.target.value)}
+        placeholder="מגניטודה"
+        type="text"
+        style={{
+          display: showMagnitodaInput ? "block" : "none",
+          position: "absolute",
+          top: "20px",
+          left: "350px",
+          zIndex: "1000",
+          height: "50px",
+          backgroundColor: "rgb(25 25 25)",
+          borderRadius: "5px",
+          width: "100px",
+          color: "white",
+        }}
+      />
+      <button
+        style={{
+          display: showMagnitodaInput ? "block" : "none",
+          position: "absolute",
+          top: "80px",
+          left: "350px",
+          zIndex: "500",
+          height: "20px",
+          backgroundColor: "rgb(25 25 25)",
+          borderRadius: "5px",
+          width: "100px",
+          color: "white",
+        }}
+        onClick={handlePredictionOnClick}
+      >
+        הפעל חיזוי
+      </button>
       <LayerDisplayer layers={layers} showLayers={true} />
     </>
   );
